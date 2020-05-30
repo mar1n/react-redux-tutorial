fcc71bae52d357f3b0c99e2ee12b05f046f4d8e0
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-31 00:05:43 +0100

    error handling

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,62 +1,57 @@
-import React from 'react'
-import './Stories.css'
-import { connect } from'react-redux'
-import { getReadableStories } from '../selectors/story'
+import React from "react";
+import "./Stories.css";
+import { connect } from "react-redux";
+import { getReadableStories, getFetchError } from "../selectors/story";
 
-import Story from './Story'
+import Story from "./Story";
 
 const COLUMNS = {
   title: {
-    label: 'Title',
-    width: '40%',
+    label: "Title",
+    width: "40%",
   },
   author: {
-    label: 'Author',
-    width: '30%',
+    label: "Author",
+    width: "30%",
   },
   comments: {
-    label: 'Comments',
-    width: '10%',
+    label: "Comments",
+    width: "10%",
   },
   points: {
-    label: 'Points',
-    width: '10%',
+    label: "Points",
+    width: "10%",
   },
   archive: {
-    width: '10%',
+    width: "10%",
   },
 };
 
-const Stories = ({ stories }) =>
+const Stories = ({ stories, error }) => (
   <div className="stories">
     <StoriesHeader columns={COLUMNS} />
 
-    {(stories || []).map(story =>
-      <Story
-        key={story.objectID}
-        story={story}
-        columns={COLUMNS}
-      />
-    )}
-  </div>
+    {error && <p className="error">Something went wrong...</p>}
 
-const StoriesHeader = ({ columns }) =>
+    {(stories || []).map((story) => (
+      <Story key={story.objectID} story={story} columns={COLUMNS} />
+    ))}
+  </div>
+);
+
+const StoriesHeader = ({ columns }) => (
   <div className="stories-header">
-    {Object.keys(columns).map(key =>
-      <span
-        key={key}
-        style={{ width: columns[key].width }}
-      >
+    {Object.keys(columns).map((key) => (
+      <span key={key} style={{ width: columns[key].width }}>
         {columns[key].label}
       </span>
-    )}
+    ))}
   </div>
+);
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   stories: getReadableStories(state),
-})
+  error: getFetchError(state)
+});
 
-
-export default connect(
-  mapStateToProps
-)(Stories)
+export default connect(mapStateToProps)(Stories);
--- src/reducers/story.js
+++ src/reducers/story.js
@@ -1,16 +1,30 @@
-import { STORIES_ADD } from "../constants/actionTypes";
-const INITIAL_STATE = []
-  
-const applyAddStories = (state, action) =>
-    action.stories
+import { STORIES_ADD, STORIES_FETCH_ERROR } from "../constants/actionTypes";
+const INITIAL_STATE = {
+  stories: [],
+  error: null,
+};
 
-  function storyReducer(state = INITIAL_STATE, action) {
-    switch(action.type) {
-        case STORIES_ADD : {
-            return applyAddStories(state, action)
-        }
-      default : return state
+const applyAddStories = (state, action) => ({
+  stories: action.stories,
+  error: null,
+});
+
+const applyFetchErrorStories = (state, action) => ({
+  stories: [],
+  error: action.error,
+})
+
+function storyReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case STORIES_ADD: {
+      return applyAddStories(state, action);
     }
+    case STORIES_FETCH_ERROR : {
+      return applyFetchErrorStories(state, action)
+    }
+    default:
+      return state;
   }
-  
-  export default storyReducer
+}
+
+export default storyReducer;

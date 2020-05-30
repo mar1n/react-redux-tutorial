573de3f75e6788b67ead86f75665f6dbc986a8f7
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-30 22:29:49 +0100

    redux and data fetching saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,18 @@
-import { createStore, applyMiddleware } from 'redux'
-import { createLogger } from 'redux-logger'
-import rootReducer from '../reducers'
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer from '../reducers';
+import rootSaga from '../sagas';
  
-const logger = createLogger()
-
+const logger = createLogger();
+const saga = createSagaMiddleware();
+ 
 const store = createStore(
   rootReducer,
   undefined,
-  applyMiddleware(logger)
+  applyMiddleware(saga, logger)
 );
  
-export default store
+saga.run(rootSaga);
+ 
+export default store;

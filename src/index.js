739fe9c377d3932d84bf9f9747b4ec29a9e460f1
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-30 21:09:30 +0100

    use selector to retrieve data and re-render

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@ import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./components/App"
 import store from "./store"
+import { getReadableStories } from './selectors/story'
 import { STORY_ARCHIVE } from "./constants/actionTypes"
 import * as serviceWorker from "./serviceWorker"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App
-      stories={store.getState().storyState}
-      onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
-    />
-    ,
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        stories={getReadableStories(store.getState())}
+        onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+      />
+      ,
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+
+store.subscribe(render)
+render()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
